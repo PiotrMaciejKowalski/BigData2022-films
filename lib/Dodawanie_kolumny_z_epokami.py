f695ab3cea73b33680ab2a1e600f96ef19e95c83
Jan Ostalski <115500640+janostalski@users.noreply.github.com>
2022-12-07 21:12:11 +0100

    Funkcja dodająca do tabeli kolumnę z okresami

---
+++ lib/Dodawanie_kolumny_z_epokami.py
@@ -0,0 +1,19 @@
+from pyspark.sql.functions import col, when
+from pyspark.sql.dataframe import DataFrame
+
+def add_epoch_column(df: DataFrame) -> DataFrame:
+  periods = [1901,1918,1926,1939,1954,1970,1985,1994,2009]
+  df_no_N = df.filter(df.startYear != "\\N")
+
+  df_periods = df_no_N.withColumn('period',
+                             when(col('startYear') <= periods[0], "1")
+                             .when(col('startYear') <= periods[1], "2")
+                             .when(col('startYear') <= periods[2], "3")
+                             .when(col('startYear') <= periods[3], "4")
+                             .when(col('startYear') <= periods[4], "5")
+                             .when(col('startYear') <= periods[5], "6")
+                             .when(col('startYear') <= periods[6], "7")
+                             .when(col('startYear') <= periods[7], "8")
+                             .when(col('startYear') <= periods[8], "9")
+                             .otherwise("10"))
+  return df_periods
