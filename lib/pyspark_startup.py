d46379f47650064874474af2bcb7d796abff7e27
Naunn <bartosz.m.lewandowski@gmail.com>
2022-12-07 19:28:37 +0100

    Zmieniono połączenie tabel

--- lib/pyspark_startup.py
+++ lib/pyspark_startup.py
@@ -2,49 +2,45 @@ import findspark
 findspark.init()
 
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import countDistinct, first
 
 def init() -> SparkSession:
     spark = SparkSession.builder.master("local[*]").getOrCreate()
     return spark
 
 def load(spark: SparkSession) -> DataFrame:
+    title_basics = spark.read.csv("title.basics.tsv.gz", sep='\t', header=True).drop("originalTitle")
+    title_seasons = spark.read.csv("title.episode.tsv.gz", sep='\t', header=True)[['parentTconst','seasonNumber']].replace(to_replace='\\N', value=None).groupBy('parentTconst').agg(countDistinct('seasonNumber'))
+    title_episode = spark.read.csv("title.episode.tsv.gz", sep='\t', header=True)[['parentTconst','episodeNumber']].replace(to_replace='\\N', value=None).groupby('parentTconst').count()
     title_ratings = spark.read.csv("title.ratings.tsv.gz", sep='\t', header=True)
-    title_principals = spark.read.csv("title.principals.tsv.gz", sep='\t',header=True)
-    title_episode = spark.read.csv("title.episode.tsv.gz", sep='\t', header=True)
-    title_crew = spark.read.csv("title.crew.tsv.gz", sep='\t', header=True)
-    title_basics = spark.read.csv("title.basics.tsv.gz", sep='\t', header=True)
-    title_akas = spark.read.csv("title.akas.tsv.gz", sep='\t', header=True)
-    name_basics = spark.read.csv("name.basics.tsv.gz", sep='\t', header=True)
+    title_principals = spark.read.csv("title.principals.tsv.gz", sep='\t',header=True).select("tconst","ordering","nconst").groupBy("tconst").pivot("ordering").agg(first("nconst"))
 
-    temp_akas = title_akas.filter(title_akas.isOriginalTitle == 1)
-    temp_akas = temp_akas.select(["titleId", "region", "language"]).distinct()
-    temp_akas = temp_akas.withColumnRenamed("titleId", "tconst")
-
-    to_print = ["title_basics", "title_ratings", "title_principals", 
-                "title_episode", "name_basics", "temp_akas"]
-
-    for p in to_print:
-        #TODO rozważyć przejście na logging (lib do robienia logów) zamiast printów
-        print(f"Dimension {p}: ({eval(p).count()}, {len(eval(p).columns)})")
-
-    data = title_basics.join(title_ratings, how="left", on="tconst")
-    print(f"\nJoined title_principals to title_basics\n" + 
-        f"Dimension: ({data.count()}, {len(data.columns)})")
-
-    data = data.join(title_principals, how="left", on="tconst")
-    print(f"Joined title_principals\n" + 
-        f"Dimension: ({data.count()}, {len(data.columns)})")
-
-    data = data.join(title_episode, how="left", on="tconst")
-    print(f"Joined title_episode\n" + 
-        f"Dimension: ({data.count()}, {len(data.columns)})")
-
-    data = data.join(name_basics, how="left", on="nconst")
-    print(f"Joined name_basics\n" + 
-        f"Dimension: ({data.count()}, {len(data.columns)})")
-
-    data = data.join(temp_akas, how="left", on="tconst")
-    print(f"Joined temp_akas\n" + 
-        f"Dimension: ({data.count()}, {len(data.columns)})")
-
+    data = title_basics.\
+        join(title_seasons,
+            title_basics.tconst == title_seasons.parentTconst,
+            how = "left").\
+            join(title_episode,
+                title_basics.tconst == title_episode.parentTconst,
+                how = "left").\
+                join(title_ratings,
+                    ["tconst"],
+                    how = "left").\
+                    drop("parentTconst").\
+                    join(title_principals,
+                            ["tconst"],
+                            how = "left").\
+                            toDF("id",
+                                "rodzaj_produkcji",
+                                "tytul",
+                                "czy_dla_doroslych",
+                                "rok_wydania_produkcji",
+                                "rok_zakonczenia_produkcji",
+                                "dlugosc_pordukcji_w_min",
+                                "gatunek",
+                                "liczba_sezonow",
+                                "liczba_wszystkich_odcinkow",
+                                "ocena",
+                                "liczba_glosow",
+                                "1","10","2","3","4","5","6","7","8","9")
+                                
     return data
