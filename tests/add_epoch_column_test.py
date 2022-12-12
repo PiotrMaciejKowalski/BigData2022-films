a0beb8c082c80247731e90cca999729a3e7970df
Jan Ostalski <115500640+janostalski@users.noreply.github.com>
2022-12-12 22:59:11 +0100

    Zmiana nazwy kolumn

--- tests/add_epoch_column_test.py
+++ tests/add_epoch_column_test.py
@@ -6,10 +6,9 @@ import pytest
 import pyspark
 
 from pyspark.sql import SparkSession, DataFrame
+from pyspark_test import assert_pyspark_df_equal
 
 from lib.add_epoch_column import add_epoch_column
-
-from pyspark_test import assert_pyspark_df_equal
 
 def test_add_epoch_column():
 
@@ -18,35 +17,33 @@ def test_add_epoch_column():
       [
           (1, 1900),
           (2, 1917),
-          (3, 1925),
+          (3, 1920),
           (4, 1938),
           (5, 1953),
-          (6, 1969),
+          (6, 1960),
           (7, 1984),
           (8, 1993),
           (9, 2008),
           (10, 2022)
       ],  
-      "id int, startYear int",
+      "id int, rok_wydania_produkcji int",
   )
 
   expect_df = spark.createDataFrame(
       [
           (1, 1900, "1"),
           (2, 1917, "2"),
-          (3, 1925, "3"),
+          (3, 1920, "3"),
           (4, 1938, "4"),
           (5, 1953, "5"),
-          (6, 1969, "6"),
+          (6, 1960, "6"),
           (7, 1984, "7"),
           (8, 1993, "8"),
           (9, 2008, "9"),
           (10, 2022, "10")
       ],  
-      "id int, startYear int, period string",
+      "id int, rok_wydania_produkcji int, period string",
   )
 
   result = add_epoch_column(df)
-
-  print(result.show())
   assert_pyspark_df_equal(result, expect_df)
