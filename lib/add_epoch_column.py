83f8e6a82c9523760bd2f0d7e6a28e7aef9fc3cf
Jan Ostalski <115500640+janostalski@users.noreply.github.com>
2022-12-12 23:07:38 +0100

    Dodanie asercji oraz zmiana nazw kolumn

--- lib/add_epoch_column.py
+++ lib/add_epoch_column.py
@@ -1,20 +1,20 @@
 from pyspark.sql.functions import col, when
-from pyspark.sql.dataframe import DataFrame
 
-def add_epoch_column(df: DataFrame) -> DataFrame:
-  #TODO dodać asercje sprawdzającą, czy startYear jest integerem
-  periods = [1901,1918,1926,1939,1954,1970,1985,1994,2009]
-  # df_no_N = df.filter(df.startYear == "\\N")
+def add_epoch_column(df: DataFrame, periods = [1901,1918,1926,1939,1954,1970,1985,1994,2009]) -> DataFrame:
+
+  assert df.filter(df.rok_wydania_produkcji == "\\N").count() == 0
+  assert dict(df.dtypes)["rok_wydania_produkcji"] == "int"
 
   df_periods = df.withColumn('period',
-                             when(col('startYear') <= periods[0], "1")
-                             .when(col('startYear') <= periods[1], "2")
-                             .when(col('startYear') <= periods[2], "3")
-                             .when(col('startYear') <= periods[3], "4")
-                             .when(col('startYear') <= periods[4], "5")
-                             .when(col('startYear') <= periods[5], "6")
-                             .when(col('startYear') <= periods[6], "7")
-                             .when(col('startYear') <= periods[7], "8")
-                             .when(col('startYear') <= periods[8], "9")
+                             when(col('rok_wydania_produkcji') <= periods[0], "1")
+                             .when(col('rok_wydania_produkcji') <= periods[1], "2")
+                             .when(col('rok_wydania_produkcji') <= periods[2], "3")
+                             .when(col('rok_wydania_produkcji') <= periods[3], "4")
+                             .when(col('rok_wydania_produkcji') <= periods[4], "5")
+                             .when(col('rok_wydania_produkcji') <= periods[5], "6")
+                             .when(col('rok_wydania_produkcji') <= periods[6], "7")
+                             .when(col('rok_wydania_produkcji') <= periods[7], "8")
+                             .when(col('rok_wydania_produkcji') <= periods[8], "9")
                              .otherwise("10"))
+   
   return df_periods
