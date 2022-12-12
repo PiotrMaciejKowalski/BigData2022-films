2d0665ed26c663ddb90d6bc02e8cfb229ab9a17d
Jan Ostalski <115500640+janostalski@users.noreply.github.com>
2022-12-12 23:29:15 +0100

    dodanie import DataFrame

--- lib/add_epoch_column.py
+++ lib/add_epoch_column.py
@@ -1,5 +1,5 @@
 from pyspark.sql.functions import col, when
-from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import DataFrame
 
 def add_epoch_column(df: DataFrame, periods = [1901,1918,1926,1939,1954,1970,1985,1994,2009]) -> DataFrame:
 
