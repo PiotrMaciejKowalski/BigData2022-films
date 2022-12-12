8f546874ab729ec63041bfcc521faddd88076f9a
Kamil <kamil.kolodziejski44@gmail.com>
2022-12-12 17:09:19 +0100

    opis outputu, obsługa wyjątków i black

--- lib/pyspark_preprocesing.py
+++ lib/pyspark_preprocesing.py
@@ -3,15 +3,31 @@ from pyspark.sql import DataFrame
 from typing import List
 
 
-def one_hot_encoding(df: DataFrame, columns: List[str]) -> DataFrame:
-    out_cols = [x + '_ohe' for x in columns]
-    out_num = [x + '_num' for x in columns]
+def one_hot_encoding(
+    df: DataFrame, columns: List[str], exception: str = "skip"
+) -> DataFrame:
+    """Funkcja zwraca sparkowy DataFrame w którym wartości w wybranych kolumnach
+    zostały zamienione na takie z kodowaniem zero-jednykowym w trybie 'sparse'.
+    Przykład: (3,[0],[1.0]) -> (1.0, 0, 0)
+    3 - długość wektora,
+    [1.0] - jaka liczb znajduje się w wektorze poza zerami,
+    [0] - pozycja na której znajduje się liczba 1.0"""
 
-    df_num = StringIndexer(inputCols=columns,
-                           outputCols=out_num).fit(df).transform(df)
+    out_cols = [x + "_ohe" for x in columns]
+    out_num = [x + "_num" for x in columns]
 
-    df_ohe = OneHotEncoder(inputCols=out_num,
-                           outputCols=out_cols).fit(df_num).transform(df_num)
+    df_num = (
+        StringIndexer(inputCols=columns, outputCols=out_num)
+        .setHandleInvalid(exception)
+        .fit(df)
+        .transform(df)
+    )
+
+    df_ohe = (
+        OneHotEncoder(inputCols=out_num, outputCols=out_cols)
+        .fit(df_num)
+        .transform(df_num)
+    )
     df_ohe = df_ohe.drop(*columns, *out_num)
 
     return df_ohe
