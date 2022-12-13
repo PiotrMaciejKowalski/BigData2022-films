3ca88c9da1bf80f3d8ff79c0240d6f8b49897a01
Kamil <kamil.kolodziejski44@gmail.com>
2022-12-13 16:36:32 +0100

    cofnięcie zmian w notatniku głównym, asercja i nowy parametr w funkcji one_hot_encoding

--- lib/pyspark_preprocesing.py
+++ lib/pyspark_preprocesing.py
@@ -1,24 +1,38 @@
 from pyspark.ml.feature import StringIndexer, OneHotEncoder
 from pyspark.sql import DataFrame
-from typing import List
+from typing import List, Literal
 
 
 def one_hot_encoding(
-    df: DataFrame, columns: List[str], exception: str = "skip"
+    df: DataFrame,
+    columns: List[str],
+    on_exception: Literal["skip", "error", "keep"] = "skip",
+    drop_cols: bool = True,
 ) -> DataFrame:
     """Funkcja zwraca sparkowy DataFrame w którym wartości w wybranych kolumnach
     zostały zamienione na takie z kodowaniem zero-jednykowym w trybie 'sparse'.
+
     Przykład: (3,[0],[1.0]) -> (1.0, 0, 0)
     3 - długość wektora,
     [1.0] - jaka liczb znajduje się w wektorze poza zerami,
-    [0] - pozycja na której znajduje się liczba 1.0"""
+    [0] - pozycja na której znajduje się liczba 1.0
+
+    :param df:              pyspark.sql.DataFrame
+    :param columns:         lista kolumn do zakodowania
+    :param on_exception:    obsługa wyjątków w setHandleInvalid()
+    :param drop_cols:       usunięcie orginalnych kolumn
+    :return:                pyspark.sql.DataFrame"""
 
     out_cols = [x + "_ohe" for x in columns]
     out_num = [x + "_num" for x in columns]
 
+    assert columns in df.columns
+    assert out_cols not in df.columns
+    assert out_num not in df.columns
+
     df_num = (
         StringIndexer(inputCols=columns, outputCols=out_num)
-        .setHandleInvalid(exception)
+        .setHandleInvalid(on_exception)
         .fit(df)
         .transform(df)
     )
@@ -28,6 +42,10 @@ def one_hot_encoding(
         .fit(df_num)
         .transform(df_num)
     )
-    df_ohe = df_ohe.drop(*columns, *out_num)
+
+    df_ohe = df_ohe.drop(*out_num)
+
+    if drop_cols:
+        df_ohe = df_ohe.drop(*columns)
 
     return df_ohe
