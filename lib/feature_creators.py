35bf393c2d25ba658e4e2f970a00f74f8703577d
Jan Ostalski <115500640+janostalski@users.noreply.github.com>
2022-12-13 23:11:58 +0100

    Zmiana nazwy pliku, utworzenie zmiennej, dodany typing

--- lib/feature_creators.py
+++ lib/feature_creators.py
@@ -1,12 +1,18 @@
-from pyspark.sql.functions import col, when
+from pyspark.sql.functions import col, when, col
 from pyspark.sql import DataFrame
+from typing import Optional,List
 
-def add_epoch_column(df: DataFrame, periods = [1901,1918,1926,1939,1954,1970,1985,1994,2009]) -> DataFrame:
+def add_epoch_column(df: DataFrame, periods: Optional[List[int]] = None) -> DataFrame:
+
+  default_periods = [1901,1918,1926,1939,1954,1970,1985,1994,2009]
+
+  if periods is None:
+    periods = default_periods
 
   assert df.filter(df.rok_wydania_produkcji == "\\N").count() == 0
   assert dict(df.dtypes)["rok_wydania_produkcji"] == "int"
 
-  df_periods = df.withColumn('period',
+  df_periods = df.withColumn('epoka',
                              when(col('rok_wydania_produkcji') <= periods[0], "1")
                              .when(col('rok_wydania_produkcji') <= periods[1], "2")
                              .when(col('rok_wydania_produkcji') <= periods[2], "3")
