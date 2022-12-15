9b50a88e113e6ff95bb7979961bfc87a4ad9db2a
Naunn <bartosz.m.lewandowski@gmail.com>
2022-12-15 22:06:28 +0100

    powrót do przeszłości

--- lib/pyspark_startup.py
+++ lib/pyspark_startup.py
@@ -10,14 +10,14 @@ def init() -> SparkSession:
     spark = SparkSession.builder.master("local[*]").getOrCreate()
     return spark
 
-def load(spark: SparkSession) -> DataFrame:
+def load(spark: SparkSession, path: str = "") -> DataFrame:
     #TODO wprowadzić porządek obiektowy (utworzenie małej metody ładującej + utworzenie klasy przechowującej tabele) oraz dodanie logów
     #TODO wykorzystac hydre do przetrzymywania sciezek oraz nazw tabel
-    title_basics = spark.read.csv("content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.basics.tsv.gz", sep="\t", header=True).drop(
+    title_basics = spark.read.csv(path+"title.basics.tsv.gz", sep="\t", header=True).drop(
         "originalTitle"
     )
     title_seasons = (
-        spark.read.csv("content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.episode.tsv.gz", sep="\t", header=True)[
+        spark.read.csv(path+"title.episode.tsv.gz", sep="\t", header=True)[
             ["parentTconst", "seasonNumber"]
         ]
         .replace(to_replace="\\N", value=None)
@@ -25,16 +25,16 @@ def load(spark: SparkSession) -> DataFrame:
         .agg(countDistinct("seasonNumber"))
     )
     title_episode = (
-        spark.read.csv("content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.episode.tsv.gz", sep="\t", header=True)[
+        spark.read.csv(path+"title.episode.tsv.gz", sep="\t", header=True)[
             ["parentTconst", "episodeNumber"]
         ]
         .replace(to_replace="\\N", value=None)
         .groupby("parentTconst")
         .count()
     )
-    title_ratings = spark.read.csv("content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.ratings.tsv.gz", sep="\t", header=True)
+    title_ratings = spark.read.csv(path+"title.ratings.tsv.gz", sep="\t", header=True)
     title_principals = (
-        spark.read.csv("content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.principals.tsv.gz", sep="\t", header=True)
+        spark.read.csv(path+"title.principals.tsv.gz", sep="\t", header=True)
         .select("tconst", "ordering", "nconst")
         .groupBy("tconst")
         .pivot("ordering")
