3bb9cd396e7892517637fdad3c56f8aa95da3047
Naunn <bartosz.m.lewandowski@gmail.com>
2022-12-15 21:56:54 +0100

    Wskazanie miejsca zapisywania danych dla pliku data_source

--- data_source.sh
+++ data_source.sh
@@ -1,7 +1,7 @@
-wget "https://datasets.imdbws.com/name.basics.tsv.gz"
-wget "https://datasets.imdbws.com/title.akas.tsv.gz"
-wget "https://datasets.imdbws.com/title.basics.tsv.gz"
-wget "https://datasets.imdbws.com/title.crew.tsv.gz"
-wget "https://datasets.imdbws.com/title.episode.tsv.gz"
-wget "https://datasets.imdbws.com/title.principals.tsv.gz"
-wget "https://datasets.imdbws.com/title.ratings.tsv.gz"
+wget "https://datasets.imdbws.com/name.basics.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/name.basics.tsv.gz
+wget "https://datasets.imdbws.com/title.akas.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.akas.tsv.gz
+wget "https://datasets.imdbws.com/title.basics.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.basics.tsv.gz
+wget "https://datasets.imdbws.com/title.crew.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.crew.tsv.gz
+wget "https://datasets.imdbws.com/title.episode.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.episode.tsv.gz
+wget "https://datasets.imdbws.com/title.principals.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.principals.tsv.gz
+wget "https://datasets.imdbws.com/title.ratings.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.ratings.tsv.gz
