e5f7095f0a6d707597da6f4ace239727adef95ed
Naunn <bartosz.m.lewandowski@gmail.com>
2022-12-16 23:32:52 +0100

    Modyfikacja basha - v2

--- data_source.sh
+++ data_source.sh
@@ -1,6 +1,7 @@
-DIR=/content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022
+DIR="/content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022"
 if [ -d "$DIR"];
 then
+    echo "downloading files into $DIR"
     wget "https://datasets.imdbws.com/name.basics.tsv.gz" -O $DIR/name.basics.tsv.gz
     wget "https://datasets.imdbws.com/title.akas.tsv.gz" -O $DIR/title.akas.tsv.gz
     wget "https://datasets.imdbws.com/title.basics.tsv.gz" -O $DIR/title.basics.tsv.gz
