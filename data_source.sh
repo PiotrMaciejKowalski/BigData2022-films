cad9d02832297fecdfa96360b337ec37c185a6cf
Naunn <bartosz.m.lewandowski@gmail.com>
2022-12-16 23:11:56 +0100

    Modyfikacja basha - v1

--- data_source.sh
+++ data_source.sh
@@ -1,7 +1,12 @@
-wget "https://datasets.imdbws.com/name.basics.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/name.basics.tsv.gz
-wget "https://datasets.imdbws.com/title.akas.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.akas.tsv.gz
-wget "https://datasets.imdbws.com/title.basics.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.basics.tsv.gz
-wget "https://datasets.imdbws.com/title.crew.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.crew.tsv.gz
-wget "https://datasets.imdbws.com/title.episode.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.episode.tsv.gz
-wget "https://datasets.imdbws.com/title.principals.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.principals.tsv.gz
-wget "https://datasets.imdbws.com/title.ratings.tsv.gz" -O /content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022/title.ratings.tsv.gz
+DIR=/content/drive/.shortcut-targets-by-id/1VcOir9FMG8LzEsUE-Q8YA79c_sV0tJwp/bigdata2022
+if [ -d "$DIR"];
+then
+    wget "https://datasets.imdbws.com/name.basics.tsv.gz" -O $DIR/name.basics.tsv.gz
+    wget "https://datasets.imdbws.com/title.akas.tsv.gz" -O $DIR/title.akas.tsv.gz
+    wget "https://datasets.imdbws.com/title.basics.tsv.gz" -O $DIR/title.basics.tsv.gz
+    wget "https://datasets.imdbws.com/title.crew.tsv.gz" -O $DIR/title.crew.tsv.gz
+    wget "https://datasets.imdbws.com/title.episode.tsv.gz" -O $DIR/title.episode.tsv.gz
+    wget "https://datasets.imdbws.com/title.principals.tsv.gz" -O $DIR/title.principals.tsv.gz
+    wget "https://datasets.imdbws.com/title.ratings.tsv.gz" -O $DIR/title.ratings.tsv.gz
+else
+    echo "$DIR directory does not exist"
