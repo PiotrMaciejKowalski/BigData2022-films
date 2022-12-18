2d6946ee45a8f4658a6a438546cfb8b75183a682
Kamil <kamil.kolodziejski44@gmail.com>
2022-12-18 15:25:59 +0100

    funkcja normalize_by_group oraz test jednostkowy dla niej

--- tests/test_pyspark_preprocesing.py
+++ tests/test_pyspark_preprocesing.py
@@ -2,7 +2,7 @@ import findspark
 
 findspark.init()
 
-from pyspark_test import assert_pyspark_df_equal
+from pyspark_test import assert_pyspark_df_equal, normalize_by_group
 from pyspark.ml.linalg import SparseVector
 from lib.pyspark_preprocesing import one_hot_encoding
 from lib.pyspark_startup import init
@@ -22,6 +22,35 @@ def test_one_hot_encoding():
             (4, SparseVector(2, {}), SparseVector(1, {0: 1.0})),
         ],
         ["inp2", "inp1_ohe", "inp3_ohe"],
+    )
+
+    assert_pyspark_df_equal(result, exp_result)
+
+
+def test_normalize_by_group():
+    spark = init()
+    test_df = spark.createDataFrame(
+        [
+            ("a", "c", 1.0, 1.0, 1.0),
+            ("a", "c", 2.0, 2.0, 1.0),
+            ("a", "c", 3.0, 1.0, 1.0),
+            ("b", "c", -1.0, 1.0, 1.0),
+            ("b", "c", 1.0, 1.0, 1.0),
+        ],
+        ["col1", "col2", "col3", "col4", "col5"],
+    )
+
+    result = normalize_by_group(spark, test_df, ["col1", "col2"], ["col3", "col4"])
+
+    exp_result = spark.createDataFrame(
+        [
+            ("a", "c", 1.0, -1.0, -0.5773502691896256),
+            ("a", "c", 1.0, 0.0, 1.1547005383792517),
+            ("a", "c", 1.0, 1.0, -0.5773502691896256),
+            ("b", "c", 1.0, -0.7071067811865475, None),
+            ("b", "c", 1.0, 0.7071067811865475, None),
+        ],
+        ["col1", "col2", "col5", "norm_col3", "norm_col4"],
     )
 
     assert_pyspark_df_equal(result, exp_result)
