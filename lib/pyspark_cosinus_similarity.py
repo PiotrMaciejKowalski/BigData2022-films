2f6cd791544edded0fde8a7d05d1d2dcd1ed1318
domkoz <kozlowski_d@hotmail.com>
2022-12-20 22:17:24 +0100

    ...

--- lib/pyspark_cosinus_similarity.py
+++ lib/pyspark_cosinus_similarity.py
@@ -1,12 +1,10 @@
 from pyspark.ml.linalg import Vectors, VectorUDT
 from pyspark.sql.functions import udf
 
-# Define a function that takes two vectors as input and returns their cosine similarity
 def cosineSimilarity(vec1, vec2):
     return float(vec1.dot(vec2) / (vec1.norm(2) * vec2.norm(2)))
 
 
     cosine_similarity_udf = udf(cosine_similarity, FloatType())
 
-    # Use the UDF to compute the cosine similarity between the two columns
     df = df.withColumn("similarity", cosine_similarity_udf("col1", "col2"))
