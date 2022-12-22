104dbb212dbe8a7c890324d21b82ed5f6ddc276a
domkoz <kozlowski_d@hotmail.com>
2022-12-22 21:05:50 +0100

    Delated model.ipynb Added typing for cosine_similarity in pyspark_cosinus_similarity

--- lib/pyspark_cosinus_similarity.py
+++ lib/pyspark_cosinus_similarity.py
@@ -1,3 +1,11 @@
 # TODO: Tets, checks, exceptions
-def cosine_similarity(vec1, vec2):
+from pyspark.ml.linalg import DenseVector
+
+
+def cosine_similarity(vec1: DenseVector, vec2: DenseVector) -> float:
+    """This function returns cosinus simalirity of two Dense Vectors.
+
+    :param vec1:              pyspark.ml.linalg.DenseVector
+    :param vec2:             pyspark.ml.linalg.DenseVector
+    :return:                float"""
     return float(vec1.dot(vec2) / (vec1.norm(2) * vec2.norm(2)))
