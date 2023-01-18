7b73b9f644b226e22ab6b003ab9327f17fee2532
werkos13 <103831552+werkos13@users.noreply.github.com>
2023-01-18 18:47:32 +0100

    Update preprocessing_function_similarity.py

--- lib/preprocessing_function_similarity.py
+++ lib/preprocessing_function_similarity.py
@@ -1,24 +1,20 @@
-from pyspark.sql.functions import monotonically_increasing_id, row_number
-from pyspark.sql.window import Window
 from typing import List
 from pyspark.sql import DataFrame
 from pyspark.ml.feature import VectorAssembler
 
-def DenseVectorToSimilarity(
+def merge_dense_vectors(
     df: DataFrame,
     columns: List[str],
     ) -> DataFrame:
-    "Funkcja, która zamienia wybrane kolumny na gotowe do wrzucenia do Similarity, a resztę pozostawia bez zmian"
+    """Funkcja zwraca sparkowy DataFrame, w którym podane w parametrach kolumny zostają skonwertowane na nową kolumnę pod nazwą "features", 
+    która jest postaci DenseVector, a reszta kolumn w podanym w parametrze DataFrame pozostaje bez zmian.
+    
+    :param df: pyspark.sql.DataFrame
+    :param columns: lista kolumn do skonwertowania
+    :return: pyspark.sql.DataFrame"""
 
-    df_similarity = df.select(columns)
-    df_rest = df.drop(*columns)
-    vecAssembler2 =  VectorAssembler(inputCols=df_similarity.columns, outputCol="features", handleInvalid="keep")
-    vecAssembler2 = vecAssembler2.transform(df_similarity)
-    df1= vecAssembler2.select(["features"])
-
-    df1=df1.withColumn('row_index', row_number().over(Window.orderBy(monotonically_increasing_id())))
-    df_rest=df_rest.withColumn('row_index', row_number().over(Window.orderBy(monotonically_increasing_id())))
-    df_to_similarity = df1.join(df_rest, on=["row_index"]).drop("row_index")
+    VecAssembler = VectorAssembler(inputCols=columns, outputCol="features").transform(df)
+    VecAssembler = VecAssembler.drop(*columns)
 
 
-    return df_to_similarity
+    return VecAssembler
