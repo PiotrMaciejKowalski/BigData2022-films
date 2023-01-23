cfe46b650cefdcad91bcd3aa582f9687ff6b341b
domkoz <kozlowski_d@hotmail.com>
2023-01-23 23:43:43 +0100

    Rewrite model.py

--- lib/model.py
+++ lib/model.py
@@ -6,36 +6,79 @@ from pyspark.sql.types import FloatType
 from lib.pyspark_matrix_similarity import cos_sim_and_iou_for_row
 
 
-def train_model(df: DataFrame, movie_name: str = None, movie_id: str = None) -> DataFrame:
-    """This function returns a DataFrame that contains finally similarity for the given movie.
-    :param df:               pyspark.sql.DataFrame
-    :param movie_name:       String
-    :param movie_id:         String
-    :return:                 pyspark.sql.DataFrame"""
-    if movie_name is None and movie_id is None:
-        raise AssertionError("You need to give movie_name or movie_id")
-    if movie_id is None:
-        movie_id = df.filter(df.tytul == movie_name).select("id").collect()[0][0]
+class Model:
+    def __init__(self, df: DataFrame):
+        self.df = df
+        self.trained_df = None
 
-    return cos_sim_and_iou_for_row(df=df, movie_id=movie_id)
+    def train(self, movie_name: str = None, movie_id: str = None) -> DataFrame:
+        """This function returns a DataFrame that contains finally similarity for the given movie.
+        :param movie_name:       String
+        :param movie_id:         String
+        :return:                 pyspark.sql.DataFrame"""
+        if movie_name is None and movie_id is None:
+            raise AssertionError("You need to give movie_name or movie_id")
+        if movie_id is None:
+            movie_id = (
+                self.df.filter(self.df.tytul == movie_name).select("id").collect()[0][0]
+            )
+
+        self.trained_df = cos_sim_and_iou_for_row(df=self.df, movie_id=movie_id)
+        return self.trained_df
+
+    def predict(self, a_param: float = 0.5, sort_results=False) -> DataFrame:
+        """This function returns a DataFrame that contains finally similarity for the given movie_id in scale form 0 to 1.
+
+        :param sort_results:     boolean
+        :param a_param:          float
+        :return:                 pyspark.sql.DataFrame"""
+
+        if a_param < 0:
+            raise AttributeError("a_param can not be negative")
+
+        add_udf = f.udf(lambda x, y: a_param * x + (1 - a_param) * y, FloatType())
+
+        self.trained_df = self.trained_df.withColumn(
+            "prediction",
+            add_udf(self.trained_df["cos_similarity"], self.trained_df["IOU"]),
+        )
+
+        if sort_results:
+            return self.trained_df.sort(desc("prediction"))
+        else:
+            return self.trained_df
 
 
-def predict(df: DataFrame, a_param: float = 0.5, sort_results=False) -> DataFrame:
-    """This function returns a DataFrame that contains finally similarity for the given movie_id in scale form 0 to 1.
-
-    :param sort_results:     boolean
-    :param a_param:          float
-    :param df:               pyspark.sql.DataFrame
-    :return:                 pyspark.sql.DataFrame"""
-
-    if a_param < 0:
-        raise AttributeError("a_param can not be negative")
-
-    add_udf = f.udf(lambda x, y: a_param * x + (1 - a_param) * y, FloatType())
-
-    df = df.withColumn("prediction", add_udf(df["cos_similarity"], df["IOU"]))
-
-    if sort_results:
-        return df.sort(desc("prediction"))
-    else:
-        return df
+# def train_model(df: DataFrame, movie_name: str = None, movie_id: str = None) -> DataFrame:
+#     """This function returns a DataFrame that contains finally similarity for the given movie.
+#     :param df:               pyspark.sql.DataFrame
+#     :param movie_name:       String
+#     :param movie_id:         String
+#     :return:                 pyspark.sql.DataFrame"""
+#     if movie_name is None and movie_id is None:
+#         raise AssertionError("You need to give movie_name or movie_id")
+#     if movie_id is None:
+#         movie_id = df.filter(df.tytul == movie_name).select("id").collect()[0][0]
+#
+#     return cos_sim_and_iou_for_row(df=df, movie_id=movie_id)
+#
+#
+# def predict(df: DataFrame, a_param: float = 0.5, sort_results=False) -> DataFrame:
+#     """This function returns a DataFrame that contains finally similarity for the given movie_id in scale form 0 to 1.
+#
+#     :param sort_results:     boolean
+#     :param a_param:          float
+#     :param df:               pyspark.sql.DataFrame
+#     :return:                 pyspark.sql.DataFrame"""
+#
+#     if a_param < 0:
+#         raise AttributeError("a_param can not be negative")
+#
+#     add_udf = f.udf(lambda x, y: a_param * x + (1 - a_param) * y, FloatType())
+#
+#     df = df.withColumn("prediction", add_udf(df["cos_similarity"], df["IOU"]))
+#
+#     if sort_results:
+#         return df.sort(desc("prediction"))
+#     else:
+#         return df
