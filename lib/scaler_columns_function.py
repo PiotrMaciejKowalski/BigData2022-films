a4d38117ff1f05f93da9ed018e58208235790b23
Kamil <kamil.kolodziejski44@gmail.com>
2023-01-24 15:41:31 +0100

    dodanie parametru drop cols do funkcji skalowania

--- lib/scaler_columns_function.py
+++ lib/scaler_columns_function.py
@@ -9,6 +9,7 @@ from pyspark.ml.feature import MinMaxScaler
 def scaler_columns(
     df: DataFrame,
     columns: List[str],
+    drop_cols: bool = False,
 ) -> DataFrame:
 
     """Funkcja zwraca sparkowy DataFrame w którym podane w parametrach kolumny zostają przeskalowane metodą MinMaxScaler i zamienione na vector type, a
@@ -28,7 +29,8 @@ def scaler_columns(
     ]
     pipeline = Pipeline(stages=assemblers + scalers)
     scaledData = pipeline.fit(df).transform(df)
-    scaledData = scaledData.drop(*columns)
+    if drop_cols:
+        scaledData = scaledData.drop(*columns)
     vectors_columns = [x + "_vec" for x in columns]
     scaledData = scaledData.drop(*vectors_columns)
 
